1a304b1dda9a62617d7d563c22bc216ee6672d4a
Nikita Kireev <kireevinvest@yandex.ru>
2022-09-29 20:33:40 +0300

    Example 006 Base

---
+++ Examples/Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+int a = 203;
+int b = 15;
+int c = 5;
+int d = 9;
+int e = 116;
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.Write(max);

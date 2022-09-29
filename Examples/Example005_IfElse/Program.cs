9d46888f338488b5ace55c69c42ecbbb09d418e4
Nikita Kireev <kireevinvest@yandex.ru>
2022-09-29 20:23:25 +0300

    Example 005 If/Else

---
+++ Examples/Example005_IfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.WriteLine("Введите ваше имя:");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "никита")
+{
+    Console.WriteLine("Рады видет вас снова, Никита!");
+}
+else
+{
+    Console.Write("Здравствуйте, ");
+    Console.Write(username);
+}

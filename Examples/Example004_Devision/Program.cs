14d0193afc28245d73901dc3e3bdd42556c72ae4
Nikita Kireev <kireevinvest@yandex.ru>
2022-09-29 19:52:23 +0300

    Example 004 Devision

---
+++ Examples/Example004_Devision/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+double numberA = 25;
+double numberB = 5;
+
+double result = numberA / numberB;
+
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+Console.WriteLine(result);

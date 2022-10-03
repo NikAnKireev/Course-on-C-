848e37e120d56034b7e9340666156b27c06c2993
Nikita Kireev <kireevinvest@yandex.ru>
2022-10-03 21:01:32 +0300

    Lesson001 Task000 Square

---
+++ Seminars/Lesson001_LanguageC#/Task000/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+Например:
+4-> 16
+3 -> 9
+7 -> 49 */
+
+using System;
+Console.Clear();
+
+Console.WriteLine("Введите число:");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+
+int square= number1 * number1;
+
+Console.WriteLine($"{number1} -> {square}");

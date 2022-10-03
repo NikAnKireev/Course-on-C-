fab1700f73933d1b9786f69396e9b3a2b96c61eb
Nikita Kireev <kireevinvest@yandex.ru>
2022-10-03 23:06:46 +0300

    Task001 Square numbers

---
+++ Seminars/Lesson001_LanguageC#/Task001/Program.cs
@@ -0,0 +1,26 @@
+﻿/* 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+a = 5; b = 25 -> да 
+a = 2 b = 10 -> нет 
+a = 9; b =-3 -> нет 
+a =-3 b = 9 -> да 
+*/
+
+using System;
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int square = number1 * number1;
+
+if (square == number2)
+{
+    Console.Write("ДА");
+}
+else
+{
+    Console.Write("НЕТ");
+}

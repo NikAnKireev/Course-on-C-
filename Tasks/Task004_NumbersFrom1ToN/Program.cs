7ccd6996e96d7a749ad977e3e4ae2fc82ce318d0
Nikita Kireev <kireevinvest@yandex.ru>
2022-10-04 00:48:40 +0300

    Task 004 Numbers from 1 to N

---
+++ Tasks/Task004_NumbersFrom1ToN/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает 
+все чётные числа от 1 до N. */
+
+using System;
+Console.Clear();
+
+Console.Write("Введите число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+while (count <= numberN)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count} ");
+    }
+    count++;
+}

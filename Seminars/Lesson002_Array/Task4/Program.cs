41b1cc116f3c78ac0a6db5e75f7546f6dc30bb68
Nikita Kireev <kireevinvest@yandex.ru>
2022-10-04 21:35:17 +0300

    Lesson 002 Array Task4

---
+++ Seminars/Lesson002_Array/Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+14 -> нет 
+46 -> нет 
+161 -> да */
+
+Console.Clear();
+
+int Prompt(string mesg)
+{
+    System.Console.WriteLine(mesg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = Prompt("Введите число: ");
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine($"{number} кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine($"{number} не кратно 7 и 23");
+}

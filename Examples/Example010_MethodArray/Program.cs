cf443ce5e391c3b3d938765464ae6189e7d7aa1d
Nikita Kireev <kireevinvest@yandex.ru>
2022-10-06 20:09:30 +0300

    example 010 method array

---
+++ Examples/Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+int[] array = { 51, 72, 13, 4, 475, 4, 67, 98, 19 };
+
+int n = array.Length; // команда Lenght передает длину массива
+int find = 4;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)  // Если индекс массива "array" совпадает с переменной "find"
+        {
+            Console.WriteLine(index);
+            break; // Если попадаются два одиноковых элемента можно прервать командой "break"
+        }
+        index++;
+
+}

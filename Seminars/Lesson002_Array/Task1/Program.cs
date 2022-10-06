098298772291c9724c02268033c9a13424653a31
Nikita Kireev <kireevinvest@yandex.ru>
2022-10-06 21:23:44 +0300

    lesson 002 array task1

---
+++ Seminars/Lesson002_Array/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+456 -> 6
+782 -> 2
+918 -> 8 */
+
+Console.Clear();
+
+System.Console.Write("Введите число: ");
+// int random = new Random().Next(100, 10000000); // Решение через заданный интервал
+int number = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(number);
+int result = number % 10;
+
+System.Console.Write($"{result} - последняя цифра числа {number}");

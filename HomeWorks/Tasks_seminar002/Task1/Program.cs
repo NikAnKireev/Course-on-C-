6660cf511c1ae92308683425d6832b75b1c8b037
Nikita Kireev <kireevinvest@yandex.ru>
2022-10-06 21:52:53 +0300

    Home works seminar 002 task1

---
+++ HomeWorks/Tasks_seminar002/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Не использовать строки для расчета.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+Console.Clear();
+
+System.Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Search(int Number)
+{
+    int result = (number % 100) / 10;
+    if (number > 99 && number < 1000) System.Console.Write($"{result} - вторая цифра числа {number}");
+else
+{
+    if (number > -1000 && number < -99) System.Console.Write($"{result * -1} - вторая цифра числа {number}");
+    else
+    {
+        System.Console.WriteLine($"Число {number} не трехзначное!!!");
+    }
+}
+return number;
+}
+
+int result = Search(number);

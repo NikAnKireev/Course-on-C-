adca8c92dee0bc57ed54ad251b21786eb769e4f9
Nikita Kireev <kireevinvest@yandex.ru>
2022-10-11 21:22:45 +0300

    Lesson 004 function task 3

---
+++ Seminars/Lesson004_function/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24 
+5 -> 120 */
+
+Console.Clear();
+int InputNumber(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(System.Console.ReadLine());
+}
+
+int MultiplyNumbers(int number)
+{
+    int multiply = 1;                           
+    for (int i = 1; i <= number; i++)      
+    {
+        multiply = multiply * i; 
+    }
+    return multiply; 
+}
+
+int number = InputNumber("Введите число -> ");
+int result = MultiplyNumbers(number);
+System.Console.WriteLine($"Факториал числа {number} -> {result}");

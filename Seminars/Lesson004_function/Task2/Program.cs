92e3beca3107b3b42decc9c82f559e5896905b4f
Nikita Kireev <kireevinvest@yandex.ru>
2022-10-11 21:10:55 +0300

    Lesson 004 function task 2

---
+++ Seminars/Lesson004_function/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5 */
+
+Console.Clear();
+
+int InputNumber(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(System.Console.ReadLine());
+}
+
+int Count(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        result++;
+    }
+    return result;
+}
+
+int number = InputNumber("Введите число -> ");
+int result = Count(number);
+System.Console.WriteLine($"{result}");

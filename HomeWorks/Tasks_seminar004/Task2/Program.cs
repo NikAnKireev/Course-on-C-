38bd2f41b5101308cb80ed76b8c3f798d14223eb
Nikita Kireev <kireevinvest@yandex.ru>
2022-10-14 20:04:43 +0300

    home work seminar 004 task 2

---
+++ HomeWorks/Tasks_seminar004/Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+Функция должна на вход принимать число, а выдавать сумму его цифр
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12 */
+
+Console.Clear();
+
+int[] ElementsCount(string messag) // Вместо команды new int [count]
+{
+    System.Console.Write(messag);
+    int number = int.Parse(Console.ReadLine());
+    int[] myArray = new int[number];
+    return myArray;
+}
+
+void FillArray(int[] values)
+{
+    for (int i = 0; i < values.Length; i++)
+    {
+        values[i] = new Random().Next(0, 10);
+        Console.Write($"{values[i]} ");
+    }
+    System.Console.Write("-> ");
+}
+
+void SumNumbers(int[] array)
+{
+    int value = array[0];
+    int sum = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        sum = value + array[i];
+        value = sum;
+    }
+    System.Console.WriteLine(value);
+}
+
+int[] array = ElementsCount("Введите количество элемента массива: ");
+FillArray(array);
+SumNumbers(array);

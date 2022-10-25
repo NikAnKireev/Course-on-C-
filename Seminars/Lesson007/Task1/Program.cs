9513dc52854116da38c138ba0e6318337700a385
nikita kireev <kireevinvest@yandex.ru>
2022-10-26 01:42:27 +0900

    seminar lesson 007 task 4

--- Seminars/Lesson007/Task1/Program.cs
+++ Seminars/Lesson007/Task1/Program.cs
@@ -14,7 +14,7 @@ for (int i = 0; i < array.GetLength(0); i++)
 {
 for (int j = 0; j < array.GetLength(1); j++)
 {
-array[i, j] = random.Next(-5, 6);
+array[i, j] = random.Next(0, 6);
 }
 }
 return array;

791f7f7e3b7f5fe5b2dd61d3cfd1cbe2f118b998
Nikita Kireev <kireevinvest@yandex.ru>
2022-11-01 13:47:34 +0300

    seminar 007 task7

---
+++ Seminars/Lesson007/Task7/Program.cs
@@ -0,0 +1,26 @@
+﻿/* ывести следующий ряд последовательности:
+
+1, 2, 2, 3, 3, 3,..., 10 */
+
+Console.Clear();
+
+int InputNumber(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void PrintCountNumber(int number)
+{
+    for (int i = 0; i <= number; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+        Console.Write($"{i}\t ");
+    }
+}
+}
+
+int number = InputNumber("Введите число: ");
+PrintCountNumber(number);

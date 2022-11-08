786aa152ac416be9ec86100dff196099ec979302
Nikita Kireev <kireevinvest@yandex.ru>
2022-11-08 21:09:52 +0300

    seminar lesson 006 tasks 1-5

---
+++ Seminars/Lesson006/Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3  -> 11
+2  -> 10 */
+
+Console.Clear();
+
+int InputNumber(string message)
+{
+    System.Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[] Binarny(int number)
+{
+    int[] array = new int[10];
+    int count = array.Length - 1;
+    while (number > 0)
+    { 
+        array[count] = number % 2;
+        number /= 2;
+        count --;
+    } 
+    return array;
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int a = InputNumber("Введите число: ");
+PrintArray(Binarny(a));
---
+++ Seminars/Lesson006/Task4/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8 */
+
+Console.Clear();
+
+int InputNumber(string message)
+{
+    System.Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[] Fibonachi(int len)
+{
+    int[] array = new int[len];
+    int temp1 = 0;
+    int temp2 = 1;
+    for (int i = 2; i < array.Length - 1; i++)
+    {
+        array[i] = temp1;
+        sum = temp1 + temp2;
+
+    }
+}
+
+// int tmp1 = 0;
+// int tmp2 = 1;
+
+// while(count  > 0)
+// {
+//    int tmp = tmp1 + tmp2;
+//    System.Console.WriteLine($"{tmp1}");
+//    tmp1 = tmp2;
+//    tmp2 = tmp;
+//    count--;
+// }

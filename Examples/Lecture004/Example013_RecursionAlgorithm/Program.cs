a0a02f9f2d797021a8a7c9dfc4f9a382fd15d022
Nikita Kireev <kireevinvest@yandex.ru>
2022-11-10 22:40:49 +0300

    lecture 004 example 013

---
+++ Examples/Lecture004/Example013_RecursionAlgorithm/Program.cs
@@ -0,0 +1,42 @@
+﻿// Создание двумерного масива (матрица)
+
+Console.Clear();
+
+string[,] table = new string[2, 5]; // создание матрицы [1, 2] где 1 - количество строк, а 2 - количество столбцов
+table[1, 2] = "слово";
+
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        Console.Write($"-{table[rows, columns]}- ");
+    }
+    Console.WriteLine();
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLongLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLongLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+FillArray(matrix);
+PrintArray(matrix);

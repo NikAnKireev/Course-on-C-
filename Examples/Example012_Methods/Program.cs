9efca6677e977bc30006b04bb6a2d906501a2ed7
Nikita Kireev <kireevinvest@yandex.ru>
2022-11-10 12:26:41 +0300

    example012 methods

---
+++ Examples/Example012_Methods/Program.cs
@@ -0,0 +1,59 @@
+﻿Console.Clear();
+
+// Вид 1 ничего не принимает и не возвращает
+void Method1()
+{
+    Console.WriteLine("Автор");
+}
+
+Method1(); //вызов метода
+
+//Вид 2 что-то принимает, но не возвращает
+void Method2(string masage)
+{
+    Console.WriteLine(masage);
+}
+
+Method2("Текст сообщения"); //Вызов метода
+
+void Method21(string masage, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(masage);
+        i++;
+    }
+}
+
+Method21("Текст", 4); //Вызов метода
+
+//ctrl+C выход из бесконечного цикла
+
+//Вид 3 что-то возвращают, но ничего не принимают
+
+int Method3()
+{
+    return DateTime.Now.Year; // вызов текущей даты
+}
+
+int year = Method3(); //передаем метод в переменную year
+Console.WriteLine(year); // выводим метод в консоль
+
+//Вид 4 что-то принимет и возвращает
+
+string Method4(int count, string c)
+{
+    int i = 0;
+    string result = String.Empty; // пустая строка или ""
+
+    while (i < count)
+    {
+        result = result + c;
+        i++;
+    }
+    return result; // возвращает переменную result
+}
+
+string res = Method4(10, "z");
+Console.WriteLine(res);

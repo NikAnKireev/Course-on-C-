fc2519cfe7d36377350999084a1a966967c7c1e3
Nikita Kireev <kireevinvest@yandex.ru>
2022-11-13 13:17:45 +0300

    lecture 004 less 004 task2

---
+++ Examples/Lecture004/Less004_Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Вычеслить факториал (N!) при помощи метода и рекурсии.
+
+Console.Clear();
+
+double Factorial(int number)
+{
+    // 1! = 1
+    // 0! = 1
+    if (number == 1 || number == 0) return 1;
+    else return number * Factorial(number - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {(Factorial(i))}");
+}
+

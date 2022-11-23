a48940b3b3ec2196d6117929bd4ddcbcf62c7261
Nikita Kireev <kireevinvest@yandex.ru>
2022-11-23 23:43:53 +0300

    seminar 009 task5 power % 2

---
+++ Seminars/Lesson009_recursion/Task5/Program.cs
@@ -0,0 +1,33 @@
+﻿/*  Определите, является ли число степенью двойки:
+N = 16 -> "Является степень двойки"
+N = 12 -> “Не является степенью двойки” */
+
+Console.Clear();
+
+int Prompt(string message)
+{
+    Console.Write(message + " > ");
+    return int.Parse(Console.ReadLine());
+}
+
+bool Power(int number)
+{
+    if (number <= 2)
+    {
+        return true;
+    }
+    return (number % 2 == 0) && (Power(number / 2));
+}
+
+int numberN = Prompt("Введите число N");
+bool result = Power(numberN);
+Console.WriteLine();
+
+if (result)
+{
+    Console.WriteLine($"N = {numberN} > Является степенью двойки!");
+}
+else
+{
+    Console.WriteLine($"N = {numberN} > Не является степенью двойки!");
+}

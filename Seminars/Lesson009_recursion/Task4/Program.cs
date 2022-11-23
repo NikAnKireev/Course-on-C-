66cafce11b08996ff3fa138aced486ac2049d4c2
Nikita Kireev <kireevinvest@yandex.ru>
2022-11-23 23:13:46 +0300

    seminar 009 task4 Power

---
+++ Seminars/Lesson009_recursion/Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Напишите программу, которая на вход принимает два числа A и B,
+и возводит число А в целую степень B с помощью рекурсии.
+
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8 */
+
+Console.Clear();
+
+int Prompt(string message)
+{
+    Console.Write(message + " > ");
+    return int.Parse(Console.ReadLine());
+}
+
+double Power(int firstNumber, int secondNumber)
+{
+    if (secondNumber <= 0)
+    {
+        return 1;
+    }
+    return firstNumber * Power(firstNumber, secondNumber - 1);
+}
+
+int numberA = Prompt("Введите число A");
+int numberB = Prompt("Введите число B");
+
+double result = Power(numberA, numberB);
+Console.WriteLine(result);

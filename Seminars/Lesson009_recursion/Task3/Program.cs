9369d6097f4cb172c0469e7de8b838c8018858a1
Nikita Kireev <kireevinvest@yandex.ru>
2022-11-23 22:48:32 +0300

    seminar 009 task3

---
+++ Seminars/Lesson009_recursion/Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+Использовать рекурсию.
+453 -> 12
+45 -> 9 */
+
+Console.Clear();
+
+int Prompt(string message)
+{
+    Console.Write(message + " > ");
+    return int.Parse(Console.ReadLine());
+}
+
+int PrintNumbers(int number)
+{
+    if (number <= 0)
+    {
+        return 0;
+    }
+    return number % 10 + PrintNumbers(number / 10);
+}
+
+int number = Prompt("Введите число");
+int sum = PrintNumbers(number);
+Console.WriteLine($"{number} > {sum}");

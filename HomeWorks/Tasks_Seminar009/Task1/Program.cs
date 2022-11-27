94ebd844cfa9b6b3404633b4a89afb9a59e8f8c8
Nikita Kireev <kireevinvest@yandex.ru>
2022-11-27 11:43:57 +0300

    homework seminar 009 task1

---
+++ HomeWorks/Tasks_Seminar009/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа 
+в промежутке от M до N с помощью рекурсии.
+
+M = 1; N = 5 -> "2, 4"
+M = 4; N = 8 -> "4, 6, 8" */
+
+Console.Clear();
+
+int Prompt(string message)
+{
+    Console.Write(message + " > ");
+    return int.Parse(Console.ReadLine());
+}
+
+void PrintNumbers(int numberM, int numberN)
+{
+    if (numberN < numberM)
+    {
+        return;
+    }
+    PrintNumbers(numberM, numberN - 1);
+    if (numberN % 2 == 0) Console.Write(numberN + " ");
+}
+
+int numbM = Prompt("Введите число M");
+int numbN = Prompt("Введите число N");
+PrintNumbers(numbM, numbN);

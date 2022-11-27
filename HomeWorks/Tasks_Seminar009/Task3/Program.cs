942230968af50b29f92d6d708e2f127f92e9a209
Nikita Kireev <kireevinvest@yandex.ru>
2022-11-27 12:34:50 +0300

    homework seminar 009 task3 Akkerman

---
+++ HomeWorks/Tasks_Seminar009/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 3, n = 2 -> A(m,n) = 29 */
+
+int Prompt(string message)
+{
+    Console.Write(message + " > ");
+    return int.Parse(Console.ReadLine());
+}
+
+int Akkerman(int valueM, int valueN)
+{
+    if (valueM == 0) return valueN + 1;
+    if (valueM > 0 && valueN == 0) return Akkerman(valueM - 1, 1);
+    else return Akkerman(valueM - 1, Akkerman(valueM, valueN - 1));
+}
+int m = Prompt("Введите число M");
+int n = Prompt("Введите число N");
+Console.WriteLine();
+Console.WriteLine($"M = {m}, N = {n} -> A(m,n) = {Akkerman(m, n)}");
